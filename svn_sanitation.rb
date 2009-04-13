54c9b8247db4374d366b148e5380f2ed56b6c4b2
andhapp <anuj@andhapp.com>
2009-04-13 17:51:26 +0100

    removed the local directory path used for testing

--- svn_sanitation.rb
+++ svn_sanitation.rb
@@ -1,3 +1,3 @@
 %w{find fileutils}.each{|x| require "#{x}"}
-root_directory_path = "C:\\Projects\\Ford projects\\DealerPieces\\TrunkFOI"
+root_directory_path = "specify_path_here"
 Find.find(root_directory_path) {|path| FileUtils.rmtree path if path =~ /.svn/ && FileTest.directory?(path) }
