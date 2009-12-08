30684b1309ec81a527d3637a41e2fb89cb8f005d
anuj  dutta <andhapp@andHapps-MacBook.local>
2009-12-09 00:43:56 +0530

    added .gitignore; added nifty shell scripts

---
+++ shell-scripts/find_ruby_files.sh
@@ -0,0 +1,2 @@
+#Command to print all the ruby files in a directory. Used for looking at all the code files in a script.  
+find . -name "*.rb" -print
---
+++ shell-scripts/loc_group_by_file.sh
@@ -0,0 +1,2 @@
+# to find the number of lines of code in a src directory
+find . -name "*.rb" | xargs wc -l
